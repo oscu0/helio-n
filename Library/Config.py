33dc291411bddaa58b3e5d9b4a648cc98b98a369
Anton Shiryaev <anton.o.shiryaev@gmail.com>
2026-01-04 12:18:30 +0300

    Accept $MACHINE override

--- Library/Config.py
+++ Library/Config.py
@@ -1,15 +1,18 @@
 from pathlib import Path
 import json
 import socket
+import os
 
 hostname = socket.gethostname()
+override_machine = os.environ.get("MACHINE")
 
 SCRIPT_DIR = Path(__file__).resolve().parent
 
 
 with open(SCRIPT_DIR / "../Config/Machine.json", "r") as f:
     machines = json.load(f)
-    machine_config = machines.get(hostname, next(iter(machines.values())))
+    key = override_machine if override_machine else hostname
+    machine_config = machines.get(key, next(iter(machines.values())))
 
 paths = {
     "fits_root": machine_config["fits_root"],
@@ -17,6 +20,9 @@ paths = {
     "hmi_root": machine_config["hmi_root"],
     "artifact_root": machine_config["artifact_root"],
 }
+
+# Ensure artifact_root exists
+Path(paths["artifact_root"]).mkdir(parents=True, exist_ok=True)
 
 apply_config = {
     "batch_size": machine_config["apply_batch_size"],
