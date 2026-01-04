cf1c44d2c50d926c804cbe38d9dc1cd47568673c
Anton Shiryaev <anton.o.shiryaev@gmail.com>
2026-01-05 02:27:38 +0300

    Set hourly df builder to false by default

--- Scripts/Make.py
+++ Scripts/Make.py
@@ -6,14 +6,10 @@ import sys
 sys.path.append("..")
 from Library.IO import prepare_dataset
 
-try:
-    hourly = sys.argv[1]
-except IndexError:
-    hourly = True
 
 prepare_dataset(
     paths["fits_root"],
     paths["masks_root"],
     hmi_root=paths["hmi_root"],
-    hourly=hourly
+    hourly=False
 )
