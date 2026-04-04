339d9a42ee2a46bcb77d4da025206f4e1b3d20f0
Anton Shiryaev <anton.o.shiryaev@gmail.com>
2026-04-04 15:04:00 +0300

    Fixed slow SW in old model

--- Models/CH_SW_Correspondence/Shugay.py
+++ Models/CH_SW_Correspondence/Shugay.py
@@ -4,7 +4,7 @@ from Library.SW.CH_SW_Model import EmpiricalCHSWModel
 
 MODEL = EmpiricalCHSWModel.from_fields(
     source_path=Path(__file__).resolve(),
-    v_min=350.0,
+    v_min=300.0,
     a=180.0,
     alpha=0.6,
 )
